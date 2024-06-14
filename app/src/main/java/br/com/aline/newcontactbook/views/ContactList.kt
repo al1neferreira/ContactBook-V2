46381683c027064ab857a3302b85a5a3d39f4203
Aline Ferreira <alinfst@gmail.com>
2024-06-14 17:16:18 -0300

    create screens files and routes in navController

---
+++ app/src/main/java/br/com/aline/newcontactbook/views/ContactList.kt
@@ -0,0 +1,16 @@
+package br.com.aline.newcontactbook.views
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ContactList(navController: NavController) {
+    
+    Column() {
+        Text(text = "Lista de contatos")
+        
+    }
+    
+}
---
+++ app/src/main/java/br/com/aline/newcontactbook/views/UpdateContact.kt
@@ -0,0 +1,9 @@
+package br.com.aline.newcontactbook.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun UpdateContact(navController: NavController) {
+    
+}
